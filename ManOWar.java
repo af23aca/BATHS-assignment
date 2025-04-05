0cc46baeae0830b7f07b3d6f5461d6c1bd2ab307
bb23abm <bb23abm@herts.ac.uk>
2025-04-06 00:51:39 +0100

    refining ship extended classes
    cleaning up code

--- ManOWar.java
+++ ManOWar.java
@@ -7,14 +7,13 @@ package wars;
 
 /**
  *
- * @author amnafarhan
+ * @author amnafarhan, barbod
  */
 public class ManOWar extends Ship {
     private int numberOfDecks;
     private int numberOfMarines;
     
-    public ManOWar (String name, String captain, int battleskill, int commissionFee, String status, 
-                    int numberOfDecks, int numberofMarines){
+    public ManOWar (String name, String captain, int battleskill, int commissionFee, String status, int numberOfDecks, int numberofMarines){
     super(name, captain, battleskill, commissionFee, status);
     numberOfDecks = this.numberOfDecks;
     numberOfMarines = this.numberOfMarines;
@@ -30,11 +29,18 @@ public int numMarines(){
     return numberOfMarines;
 }
 
+@Override
+public boolean canFight(EncounterType type){
+    return type == EncounterType.BATTLE || type == EncounterType.BLOACKADE;
+}
+
 
     
  @Override
     public String toString() {
-        return super.toString() + "\nNumber of decks: " + numberOfDecks +
+        return super.toString() + 
+                "\nType: MAN-0-WAR" +
+                "\nNumber of decks: " + numberOfDecks +
                 "\nNumber of Marines: " + numberOfMarines;
     }
       
